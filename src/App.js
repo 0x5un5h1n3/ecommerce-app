1b2f583e4e7c4a784ba2ff879a41325ed50fb46a
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2024-11-16 16:17:03 +0530

    Add authentication to App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
@@ -7,14 +7,27 @@ import RegisterForm from "./components/RegisterForm";
 import LoginForm from "./components/LoginForm";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (user) => {
+    setUser(user);
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+  };
+
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar user={user} handleLogout={handleLogout} />
       <Switch>
         <Route path="/" exact component={ProductList} />
         <Route path="/cart" component={Cart} />
         <Route path="/register" component={RegisterForm} />
-        <Route path="/login" component={LoginForm} />
+        <Route
+          path="/login"
+          render={(props) => <LoginForm {...props} handleLogin={handleLogin} />}
+        />
       </Switch>
     </BrowserRouter>
   );
