c4819720ba2f598e0195d1f99b96a4a304fa5ef7
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2024-11-16 16:17:27 +0530

    Add authentication to Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 
-const Navbar = () => {
+const Navbar = ({ user, handleLogout }) => {
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" component="h1">
           Ecommerce App
         </Typography>
+        {user ? (
+          <Button color="inherit" onClick={handleLogout}>
+            Logout
+          </Button>
+        ) : (
+          <Button color="inherit" href="/login">
+            Login
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
   );
