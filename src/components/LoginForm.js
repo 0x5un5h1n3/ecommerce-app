5f7217644214c66e25000dd4c6592b5936585e75
Ox5un5h1n3 <5un5h1n34096@protonmail.com>
2024-11-16 16:12:03 +0530

    Create LoginForm component

---
+++ src/components/LoginForm.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { TextField, Button } from "@mui/material";
+
+const LoginForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email").required("Email is required"),
+      password: Yup.string().required("Password is required"),
+    }),
+    onSubmit: (values, { setSubmitting }) => {
+      console.log(values);
+      setSubmitting(false);
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <TextField
+        id="email"
+        name="email"
+        label="Email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        error={formik.touched.email && Boolean(formik.errors.email)}
+        helperText={formik.touched.email && formik.errors.email}
+      />
+      <TextField
+        id="password"
+        name="password"
+        label="Password"
+        type="password"
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        error={formik.touched.password && Boolean(formik.errors.password)}
+        helperText={formik.touched.password && formik.errors.password}
+      />
+      <Button type="submit" variant="contained">
+        Login
+      </Button>
+    </form>
+  );
+};
+
+export default LoginForm;
